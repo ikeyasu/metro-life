bb224270cd7ad7756d9265fcef07e41ffb3b9b6a
ikeyasu <ikeyasu@gmail.com>
2014-10-18 08:34:48 +0900

    Support continus deploy to heroku
    See
    http://www.parsed.io/yeoman-travis-ci-heroku/

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,7 +379,7 @@ module.exports = function (grunt) {
       },
       heroku: {
         options: {
-          remote: 'heroku',
+          remote: 'git@heroku.com:metro-life-dev.git',
           branch: 'master'
         }
       },
