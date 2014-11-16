5b4d42bcd768615e32e1917f762995a6930f7c2c
ikeyasu <ikeyasu@gmail.com>
2014-11-17 00:46:46 +0900

    Fix. show train time table

--- server/api/tokyometro/station/index.js
+++ server/api/tokyometro/station/index.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.get('/:station', controller.show);
 router.get('/nearby/:station', controller.nearby);
 router.get('/raildirection/:station', controller.raildirection);
+router.get('/timetable/:station/:direction', controller.timetable);
+router.get('/timetable/:station/:direction/:type', controller.timetable);
 
 module.exports = router;
